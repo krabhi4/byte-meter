e00de48c4e21cb5c9eb6ed1647c3287ddee2c90b
krabhi4 <friskabhi@gmail.com>
2024-11-14 20:01:36 +0530

    Refactor main elements in Home and SidebarInset components for semantic structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import HomeForm from '@/components/page/home-form';
 
 export default function Home() {
   return (
-    <div className="flex p-4 flex-col w-full h-full items-center justify-center">
+    <main
+      role="main"
+      className="flex p-4 flex-col w-full h-full items-center justify-center"
+    >
       <HomeForm />
-    </div>
+    </main>
   );
 }
